f74826ad1a29d9ad9be52995e66f529fe401e060
KleachiM <werf121@rambler.ru>
2021-01-10 21:25:24 +0300

    Commit

---
+++ advertisements/views.py
@@ -0,0 +1,32 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated, BasePermission
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+
+
+class DelPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user == view.queryset[0].creator
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    filter_backends = [DjangoFilterBackend]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated()]
+
+        if self.action == 'destroy':
+            return [DelPermission()]
+        return []
+
+
